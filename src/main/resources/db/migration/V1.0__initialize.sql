958009d28fee8dbd8e48528254d14bc511e1d65c
Kenneth Gottschalk de Azevedo <kenneth.azevedo@luizalabs.com>
2021-08-27 00:30:02 -0300

    add sql file

---
+++ src/main/resources/db/migration/V1.0__initialize.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE `character`
+(
+   id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+   name VARCHAR NOT NULL,
+   description VARCHAR,
+   created_at TIMESTAMP NOT NULL,
+   updated_at TIMESTAMP
+);
+
+CREATE INDEX character_name ON `character` (name);
