45bf22021b01e9067ded478239565de3598104f9
Kenneth Gottschalk de Azevedo <kenneth.azevedo@luizalabs.com>
2021-08-27 00:16:48 -0300

    first commit

---
+++ src/main/java/com/luizalabs/spongebob/SpongebobApplication.java
@@ -0,0 +1,13 @@
+package com.luizalabs.spongebob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpongeBobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpongeBobApplication.class, args);
+    }
+
+}
